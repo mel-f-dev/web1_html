a9d4c6eb2cc2762fa65eacc9e939b09b0ce56a11
Mel <54912463+mel-f-dev@users.noreply.github.com>
2020-04-24 16:43:27 +0900

    jquery library

--- colors.js
+++ colors.js
@@ -1,21 +1,24 @@
 var Links = {
     setColor: function(color) {
-      var alist = document.querySelectorAll("a");
-      i = 0;
-      while (i < alist.length) {
-        alist[i].style.color = color;
-        i = i + 1;
-      }
+      // var alist = document.querySelectorAll("a");
+      // i = 0;
+      // while (i < alist.length) {
+      //   alist[i].style.color = color;
+      //   i = i + 1;
+      // }
+      $('a').css('color', color);
     }
   }
   
-  var Body = {
+var Body = {
     setColor: function (color) {
-      document.querySelector("body").style.Color = color;
+      // document.querySelector("body").style.color = color;
+      $('body').css('color', color);
     },
     setBackgroundColor: function (color) {
-      document.querySelector("body").style.backgroundColor = color;
-    },
+      // document.querySelector("body").style.backgroundColor = color;
+      $('body').css('backgroundColor', color);
+    }
   }
 
   function darkmodeHandler(self) {
